9352f97da7ae55c3007e2817b4310bf508c31bb3
jokereven <zjing125.cn@gmail.com>
2022-08-01 21:57:11 +0800

    feat: 完成基础布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
+import { LayoutWrapper } from "layout";
 import React from "react";
-import logo from "./logo.svg";
+import "antd/dist/antd.min.css";
 import "./App.css";
+import "./scrollbar.css";
+
+/* 全局样式 */
+import { GlobalStyle } from "./style";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* 全局样式 */}
+      <GlobalStyle />
+      <LayoutWrapper></LayoutWrapper>
     </div>
   );
 }
---
+++ src/layout/style.js
@@ -0,0 +1,3 @@
+import styled from "styled-components";
+
+export const LayoutD = styled.div``;
